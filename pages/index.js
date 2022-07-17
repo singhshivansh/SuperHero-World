06e984d2a680feaaf99a250e5e32c7643ce60a95
unknown <singhshivansh12@gmail.com>
2022-07-17 18:06:13 +0530

    search functionality

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import { useState ,useEffect } from 'react';
 import {data} from '../data/data.js';
 
 const index = () => {
-  const char = [720, 70, 644, 346, 149];
+  const char = [720, 70, 644, 346, 149, 659];
   const [featuredCharacters, setfeaturedCharacters] = useState([]);
+  const [searchedCharacters, setsearchedCharacters] = useState([]);
+  const [search, setsearch] = useState('');
+
   useEffect(() => {
     console.log(char.includes(346));
     data.filter(item => {
@@ -11,7 +14,27 @@ const index = () => {
         setfeaturedCharacters(prevState => [...prevState, item]);
       }
     })
+
+    var randoms = [...Array(10)].map(() => Math.floor(Math.random() * 700));
+    data.filter(item=>{
+      if(randoms.includes(parseInt(item))){
+        setsearchedCharacters(prevState => [...prevState, item]);
+      }
+    })
   }, []);
+
+
+  const handleChange = (e) => {
+    setsearch(e.target.value);
+    let arr = [];
+    data.filter(item => {
+      if(item[1].toLowerCase().includes(e.target.value.toLowerCase())){
+        arr.push(item);
+        // setsearchedCharacters(prevState => [...prevState, item]);
+      }
+    })
+    setsearchedCharacters(arr);
+  }
 
   return (<>
     <div className="bg-slate-600 flex py-5 justify-center">
@@ -25,13 +48,46 @@ const index = () => {
         <hr className="bg-slate-500 mt-3 h-1"></hr>
         <div>
           <div className="flex flex-wrap justify-center">
-            {featuredCharacters.slice(0,5).map((character, index) => {
+            {featuredCharacters.slice(0,6).map((character, index) => {
               return (
               <>
-                <div key={index} className="lg:w-1/5 md:w-1/4 sm:w-2/4 h-auto p-3">
+                <div key={index} className="lg:w-1/6 md:w-1/4 sm:w-2/4 h-64 p-3">
+                  <div className="bg-white rounded-md shadow-md ">
+                    <div className="flex justify-center w-full">
+                      <div className='flex flex-col justify-end'>
+                        <img src={character[2]} alt="character" className="h-48" />
+                        <h3 className="text-center text-xl">{character[1]}</h3>
+                      </div>
+                    </div>
+                    <div className="flex justify-center">
+                    </div>
+                  </div>
+                </div>
+              </>)
+              })}
+            </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="flex justify-center my-3">
+        <input className="bg-slate-200 px-2 rounded-md mr-3" placeholder="Enter the Character Name" onChange={handleChange}/>
+        <button className="py-1 px-3 bg-teal-600 rounded-md text-white">Search</button>
+    </div>
+
+    <div className="flex justify-center ">
+      <div className=" w-5/6 ">
+        <h3 className="text-4xl ">SEARCHED CHARACTERS</h3>
+        <hr className="bg-slate-500 mt-3 h-1"></hr>
+        <div className='min-h-full'>
+          <div className="flex flex-wrap justify-center " style={{'minHeight' : '500px'}}>
+            {searchedCharacters.slice(0, 12).map((character, index) => {
+              return (
+              <>
+                <div key={index} className="lg:w-1/6 md:w-1/4 sm:w-2/4 p-3 h-64">
                   <div className="bg-white rounded-md shadow-md">
                     <div className="flex justify-center">
-                      <img src={character[2]} alt="character" className="h-64" />
+                      <img src={character[2]} alt="character" className="h-48" />
                     </div>
                     <div className="flex justify-center">
                       <h3 className="text-center text-xl">{character[1]}</h3>
@@ -45,12 +101,10 @@ const index = () => {
       </div>
     </div>
 
-    <div className="flex justify-center my-3">
-        <input className="bg-slate-200 px-2 rounded-md mr-3" placeholder="Enter the Character Name" />
-        <button className="py-1 px-3 bg-teal-600 rounded-md text-white">Search</button>
-    </div>
+    
 
     
+
   </>
   )
 }
